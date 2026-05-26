87873860e4fdd6a5804feb794e5dc98a73f6687e
thierry <thierry.quevedo@unesp.br>
2026-05-26 12:02:29 -0300

    metodos editar deletar salvar

--- src/java/com/template/MainController.java
+++ src/java/com/template/MainController.java
@@ -1,10 +1,13 @@
 package com.template;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
 
 public class MainController
 {
@@ -23,25 +26,65 @@ public class MainController
     @FXML private TableColumn<DesenhoDTO,String> colAnoLancamento;
     @FXML private TableColumn<DesenhoDTO,String> colProtagonista;
     @FXML private TableColumn<DesenhoDTO,String> colCriador;
-    @FXML
-    private void btnSalvarAction(ActionEvent event){
+    @FXML private void carregarDesenhos(){
+        DesenhoDAO objDesenhoDAO = new DesenhoDAO();
+        ArrayList<DesenhoDTO> listaDesenhos = objDesenhoDAO.selecionarDesenho();
+        tblDesenho.setItems(FXCollections.observableArrayList(listaDesenhos));
+    }
+    @FXML private void btnSalvarAction(ActionEvent event){
         int id = Integer.parseInt(txtId.getText());
         String nome = txtNome.getText();
         int anoLancamento = Integer.parseInt(txtAnoLancamento.getText());
         String protagonista = txtProtagonista.getText();
         String criador = txtCriador.getText();
 
+        DesenhoDTO objdesenhodto = new DesenhoDTO();
+        objdesenhodto.setNome(nome);
+        objdesenhodto.setAnoLancamento(anoLancamento);
+        objdesenhodto.setCriador(criador);
+        objdesenhodto.setProtagonista(protagonista);
+
+        DesenhoDAO objdesenhodao = new DesenhoDAO();
+        objdesenhodao.cadastrarDesenho(objdesenhodto);
+
+        carregarDesenhos();
     }
-    @FXML
-    private void btnLimparAction(ActionEvent event){
+    @FXML private void btnLimparAction(ActionEvent event){
         txtId.setText("");
         txtAnoLancamento.setText("");
         txtProtagonista.setText("");
         txtNome.setText("");
         txtCriador.setText("");
     }
-    @FXML
-    private void initialize()
+    @FXML private void btnEditarAction(){
+        DesenhoDTO desenhodto = tblDesenho.getSelectionModel().getSelectedItem();
+
+        if (desenhodto != null) {
+            desenhodto.setNome(txtNome.getText());
+            desenhodto.setAnoLancamento(Integer.parseInt(txtAnoLancamento.getText()));
+            desenhodto.setProtagonista(txtProtagonista.getText());
+            desenhodto.setCriador(txtCriador.getText());
+
+
+            DesenhoDAO objdesenhodao = new DesenhoDAO();
+            objdesenhodao.atualizarDesenho(desenhodto);
+
+            carregarDesenhos();
+            btnLimparAction(null);
+        }
+    }
+    @FXML private void btnDeletarAction(){
+        DesenhoDTO desenhodto = tblDesenho.getSelectionModel().getSelectedItem();
+
+        if (desenhodto != null) {
+            DesenhoDAO objdesenhodao = new DesenhoDAO();
+            objdesenhodao.deletarDesenho(desenhodto);
+
+            carregarDesenhos();
+            btnLimparAction(null);
+        }
+    }
+    @FXML private void initialize()
     {
         System.out.println("FXML loaded successfully!");
     }
